284ded658be5fd333b8543f66b027808aca77b1e
RamharshDandekar <ramsdandekar@gmail.com>
2024-09-29 17:32:13 +0530

    on-page SEO optimization done

--- src/App.js
+++ src/App.js
@@ -19,42 +19,41 @@ import Footer from './components/Footer';
 
 const App = () => {
     const circlesData = [
-    { text: '0.3+', info: 'Years' },
-    { text: '10+', info: 'Events' },
-    { text: '1K+', info: 'Community <br> Members' }, 
-    { text: '200+', info: 'Attendees' },
-];
+        { text: '0.3+', info: 'Years' },
+        { text: '10+', info: 'Events' },
+        { text: '1K+', info: 'Community <br> Members' }, 
+        { text: '200+', info: 'Attendees' },
+    ];
 
     const technologies = [
         { name: 'Android', iconClass: 'android', info: 'Info about Android' },
         { name: 'Flutter', iconClass: 'flutter', info: 'Info about Flutter' },
         { name: 'Cloud', iconClass: 'cloud', info: 'Info about Cloud' },
-        { name: 'IT Networking', iconClass: 'IT', info: 'Info about IT Networking' },       
-        
-      // Add more technologies here
+        { name: 'IT Networking', iconClass: 'IT', info: 'Info about IT Networking' },
+        // Add more technologies here
     ];
 
     return (
         <div className="App">
             <Helmet>
-                <title>Jai Shri Ram</title>
-                <meta name="description" content="A brief description of your website and what it offers." />
-                <meta name="keywords" content="real estate, community, technology, events" />
-                <meta name="author" content="Your Name" />
+                <title>GDG RBU Chapter - Google Developer Group</title>
+                <meta name="description" content="Join the GDG RBU Chapter to engage with fellow tech enthusiasts and explore Google developer technologies." />
+                <meta name="keywords" content="Google Developer Group, GDG RBU, community engagement, technology, Android, Flutter, Cloud, IT Networking" />
+                <meta name="author" content="GDG RBU Chapter" />
             </Helmet>
             <Navbar />
             <HeroSection />
             <ScrollDownIcon />
             <SubHeading 
                 title="Our Mission" 
-                borderColor1="yellow" 
-                borderColor2="orange" 
+                borderColor1="blue" 
+                borderColor2="purple" 
                 textColor1="white" 
-                textColor2="yellow" 
+                textColor2="blue" 
             />
             <InfoRight 
                 imagePath={SubImage1} 
-                missionText={`Our mission involves community engagement, leadership development,\n building strong tech foundation, while enabling all tech enthusiasts to contribute,\n to the global society.`}
+                missionText={`Our mission involves community engagement, leadership development, building a strong tech foundation, while enabling all tech enthusiasts to contribute to the global society.`}
             />
             <SubHeading 
                 title="Our Perspective" 
@@ -64,7 +63,7 @@ const App = () => {
                 textColor2="green" 
             />
             <InfoLeft 
-                missionText={`We’re a community-driven initiative aiming to bridge the gap between research and practice , develop evolutionary thinking and network throughout the process. We believe in connecting fellow developers , spreading stimulative ideas and working for a solution driven team.`}
+                missionText={`We’re a community-driven initiative aiming to bridge the gap between research and practice, develop evolutionary thinking, and network throughout the process. We believe in connecting fellow developers, spreading stimulative ideas, and working for a solution-driven team.`}
                 imagePath={SubImage2} 
             />
             <SubHeading 
@@ -76,28 +75,26 @@ const App = () => {
             />
             <InfoRight 
                 imagePath={SubImage3} 
-                missionText={`Our club helps students to connect, learn, empower and grow. Teamwork, innovative thinking, communication, and leading with solutions is what helps us achieve new heights.
-The entire team works in coordination, to inspire and motivate the upcoming coding community to evolve their skills and broaden their horizons of knowledge.`}
+                missionText={`Our club helps students to connect, learn, empower, and grow. Teamwork, innovative thinking, communication, and leading with solutions help us achieve new heights. The entire team works in coordination to inspire and motivate the upcoming coding community to evolve their skills and broaden their horizons of knowledge.`}
             />
-        <br></br>
-        <br></br>
+            <br />
             <div>
                 <AnimatedCircles circlesData={circlesData} />
             </div>
             <div>
                 <TechnologyCards technologies={technologies} />
-        </div>
-        <SubHeading 
+            </div>
+            <SubHeading 
                 title="FAQ" 
                 borderColor1="red" 
                 borderColor2="yellow" 
                 textColor1="white" 
                 textColor2="red" 
             />
-        <FAQ></FAQ>
-        <Footer></Footer>
+            <FAQ />
+            <Footer />
         </div>
     );
-}
+};
 
 export default App;
